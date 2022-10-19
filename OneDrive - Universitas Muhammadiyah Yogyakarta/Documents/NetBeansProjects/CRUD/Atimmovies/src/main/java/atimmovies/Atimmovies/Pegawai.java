1b95551862244e84c0256d8407ea9507b8973c60
AzizFathoni <azzizfathoni@gmail.com>
2022-10-19 14:14:32 +0700

    Add Entity

--- OneDrive - Universitas Muhammadiyah Yogyakarta/Documents/NetBeansProjects/CRUD/Atimmovies/src/main/java/atimmovies/Atimmovies/Pegawai.java
+++ OneDrive - Universitas Muhammadiyah Yogyakarta/Documents/NetBeansProjects/CRUD/Atimmovies/src/main/java/atimmovies/Atimmovies/Pegawai.java
@@ -5,10 +5,128 @@
  */
 package atimmovies.Atimmovies;
 
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  *
  * @author Lenovo
  */
-public class Pegawai {
+@Entity
+@Table(name = "pegawai")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Pegawai.findAll", query = "SELECT p FROM Pegawai p"),
+    @NamedQuery(name = "Pegawai.findByIdPegawai", query = "SELECT p FROM Pegawai p WHERE p.idPegawai = :idPegawai"),
+    @NamedQuery(name = "Pegawai.findByNamaPegawai", query = "SELECT p FROM Pegawai p WHERE p.namaPegawai = :namaPegawai"),
+    @NamedQuery(name = "Pegawai.findByAlamat", query = "SELECT p FROM Pegawai p WHERE p.alamat = :alamat"),
+    @NamedQuery(name = "Pegawai.findByNoTelepon", query = "SELECT p FROM Pegawai p WHERE p.noTelepon = :noTelepon")})
+public class Pegawai implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "Id_Pegawai")
+    private String idPegawai;
+    @Basic(optional = false)
+    @Column(name = "Nama_Pegawai")
+    private String namaPegawai;
+    @Basic(optional = false)
+    @Column(name = "Alamat")
+    private String alamat;
+    @Basic(optional = false)
+    @Column(name = "no_telepon")
+    private String noTelepon;
+    @JoinColumn(name = "Id_Pegawai", referencedColumnName = "id_pelanggan", insertable = false, updatable = false)
+    @OneToOne(optional = false)
+    private Pelanggan pelanggan;
+
+    public Pegawai() {
+    }
+
+    public Pegawai(String idPegawai) {
+        this.idPegawai = idPegawai;
+    }
+
+    public Pegawai(String idPegawai, String namaPegawai, String alamat, String noTelepon) {
+        this.idPegawai = idPegawai;
+        this.namaPegawai = namaPegawai;
+        this.alamat = alamat;
+        this.noTelepon = noTelepon;
+    }
+
+    public String getIdPegawai() {
+        return idPegawai;
+    }
+
+    public void setIdPegawai(String idPegawai) {
+        this.idPegawai = idPegawai;
+    }
+
+    public String getNamaPegawai() {
+        return namaPegawai;
+    }
+
+    public void setNamaPegawai(String namaPegawai) {
+        this.namaPegawai = namaPegawai;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getNoTelepon() {
+        return noTelepon;
+    }
+
+    public void setNoTelepon(String noTelepon) {
+        this.noTelepon = noTelepon;
+    }
+
+    public Pelanggan getPelanggan() {
+        return pelanggan;
+    }
+
+    public void setPelanggan(Pelanggan pelanggan) {
+        this.pelanggan = pelanggan;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idPegawai != null ? idPegawai.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Pegawai)) {
+            return false;
+        }
+        Pegawai other = (Pegawai) object;
+        if ((this.idPegawai == null && other.idPegawai != null) || (this.idPegawai != null && !this.idPegawai.equals(other.idPegawai))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "atimmovies.Atimmovies.Pegawai[ idPegawai=" + idPegawai + " ]";
+    }
     
 }
--- OneDrive - Universitas Muhammadiyah Yogyakarta/Documents/NetBeansProjects/CRUD/Atimmovies/src/main/java/atimmovies/Atimmovies/Transaksi.java
+++ OneDrive - Universitas Muhammadiyah Yogyakarta/Documents/NetBeansProjects/CRUD/Atimmovies/src/main/java/atimmovies/Atimmovies/Transaksi.java
@@ -5,10 +5,156 @@
  */
 package atimmovies.Atimmovies;
 
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  *
  * @author Lenovo
  */
-public class Transaksi {
+@Entity
+@Table(name = "transaksi")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Transaksi.findAll", query = "SELECT t FROM Transaksi t"),
+    @NamedQuery(name = "Transaksi.findByIdtransaksi", query = "SELECT t FROM Transaksi t WHERE t.idtransaksi = :idtransaksi"),
+    @NamedQuery(name = "Transaksi.findByIdpelanggan", query = "SELECT t FROM Transaksi t WHERE t.idpelanggan = :idpelanggan"),
+    @NamedQuery(name = "Transaksi.findByIdcd", query = "SELECT t FROM Transaksi t WHERE t.idcd = :idcd"),
+    @NamedQuery(name = "Transaksi.findByTglPinjam", query = "SELECT t FROM Transaksi t WHERE t.tglPinjam = :tglPinjam"),
+    @NamedQuery(name = "Transaksi.findByBiaya", query = "SELECT t FROM Transaksi t WHERE t.biaya = :biaya")})
+public class Transaksi implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "Id_transaksi")
+    private String idtransaksi;
+    @Basic(optional = false)
+    @Column(name = "Id_pelanggan")
+    private String idpelanggan;
+    @Basic(optional = false)
+    @Column(name = "Id_cd")
+    private String idcd;
+    @Basic(optional = false)
+    @Column(name = "tgl_pinjam")
+    @Temporal(TemporalType.DATE)
+    private Date tglPinjam;
+    @Basic(optional = false)
+    @Column(name = "biaya")
+    private String biaya;
+    @JoinColumn(name = "Id_transaksi", referencedColumnName = "Id_CD", insertable = false, updatable = false)
+    @OneToOne(optional = false)
+    private Dvd dvd;
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "transaksi")
+    private Pelanggan pelanggan;
+
+    public Transaksi() {
+    }
+
+    public Transaksi(String idtransaksi) {
+        this.idtransaksi = idtransaksi;
+    }
+
+    public Transaksi(String idtransaksi, String idpelanggan, String idcd, Date tglPinjam, String biaya) {
+        this.idtransaksi = idtransaksi;
+        this.idpelanggan = idpelanggan;
+        this.idcd = idcd;
+        this.tglPinjam = tglPinjam;
+        this.biaya = biaya;
+    }
+
+    public String getIdtransaksi() {
+        return idtransaksi;
+    }
+
+    public void setIdtransaksi(String idtransaksi) {
+        this.idtransaksi = idtransaksi;
+    }
+
+    public String getIdpelanggan() {
+        return idpelanggan;
+    }
+
+    public void setIdpelanggan(String idpelanggan) {
+        this.idpelanggan = idpelanggan;
+    }
+
+    public String getIdcd() {
+        return idcd;
+    }
+
+    public void setIdcd(String idcd) {
+        this.idcd = idcd;
+    }
+
+    public Date getTglPinjam() {
+        return tglPinjam;
+    }
+
+    public void setTglPinjam(Date tglPinjam) {
+        this.tglPinjam = tglPinjam;
+    }
+
+    public String getBiaya() {
+        return biaya;
+    }
+
+    public void setBiaya(String biaya) {
+        this.biaya = biaya;
+    }
+
+    public Dvd getDvd() {
+        return dvd;
+    }
+
+    public void setDvd(Dvd dvd) {
+        this.dvd = dvd;
+    }
+
+    public Pelanggan getPelanggan() {
+        return pelanggan;
+    }
+
+    public void setPelanggan(Pelanggan pelanggan) {
+        this.pelanggan = pelanggan;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idtransaksi != null ? idtransaksi.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Transaksi)) {
+            return false;
+        }
+        Transaksi other = (Transaksi) object;
+        if ((this.idtransaksi == null && other.idtransaksi != null) || (this.idtransaksi != null && !this.idtransaksi.equals(other.idtransaksi))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "atimmovies.Atimmovies.Transaksi[ idtransaksi=" + idtransaksi + " ]";
+    }
     
 }
