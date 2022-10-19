cbb512e7f82d96c31e9988a5401ca4487548093f
AzizFathoni <azzizfathoni@gmail.com>
2022-10-19 14:18:33 +0700

    Add Entity

--- OneDrive - Universitas Muhammadiyah Yogyakarta/Documents/NetBeansProjects/CRUD/Atimmovies/src/main/java/atimmovies/Atimmovies/PegawaiJpaController.java
+++ OneDrive - Universitas Muhammadiyah Yogyakarta/Documents/NetBeansProjects/CRUD/Atimmovies/src/main/java/atimmovies/Atimmovies/PegawaiJpaController.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
@@ -27,7 +28,12 @@ public class PegawaiJpaController implements Serializable {
     public PegawaiJpaController(EntityManagerFactory emf) {
         this.emf = emf;
     }
-    private EntityManagerFactory emf = null;
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory(" atimmovies_Atimmovies_jar_0.0.1-SNAPSHOTPU ");
+
+    public PegawaiJpaController() {
+    }
+    
+    
 
     public EntityManager getEntityManager() {
         return emf.createEntityManager();
